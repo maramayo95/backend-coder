695e0a02be7b2cda06115d243539912ca814ae6a
Matias <matiasaramayo@hotmail.com>
2022-02-20 19:33:52 -0300

    desafio terminado

---
+++ desafio-02/desafio-02.js
@@ -0,0 +1,124 @@
+const fs = require('fs')
+const path = require('path')
+const uuidv4 = require("uuid/v4")
+
+class Contenedor {
+    constructor(filename) {
+        this.filename = filename;
+    }
+
+    async save(element) {
+        try {
+            const parsedFile = await this.getParsedFileOrCreateIfNotExists();
+
+            element.id = uuidv4();
+            parsedFile.push(element);
+
+            await fs.promises.writeFile(this.filename, JSON.stringify(parsedFile), "utf-8");
+
+            return element.id;
+        } catch (error) {
+            throw new Error(`Error :  ${error.message}`);
+        }
+    }
+
+    async getById(id) {
+        try {
+            file = await fs.promises.readFile(this.filename, "utf-8");
+            const parsedFile = JSON.parse(file);
+            const element = parsedFile.find(el => el.id === id);
+
+            if (!element) throw new Error(`Element with ID ${id} doesn't exist`);
+
+            const { title, price, thumbnail } = el;
+            return { title, price, thumbnail }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async getAll() {
+        try {
+            const file = await fs.promises.readFile(this.filename, "utf-8");
+            const parsedFile = JSON.parse(file);
+
+            return parsedFile.map(el => {
+                const { title, price, thumbnail } = el;
+                return { title, price, thumbnail };
+            })
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async deleteById(id) {
+        try {
+            const file = await fs.promises.readFile(this.filename, "utf-8");
+            const parsedFile = JSON.parse(file);
+            const element = parsedFile.find(el => el.id === id);
+
+            if (!element) throw new Error(`Element with ID ${id} doesn't exist`);
+
+            const filteredArray = parsedFile.filter(el => el.id !== id);
+            await fs.promises.writeFile(this.filename, JSON.stringify(filteredArray));
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async deleteAll() {
+        try {
+            await fs.promises.writeFile(this.filename, JSON.stringify('[]'));
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async getParsedFileOrCreateIfNotExists() {
+        let file;
+
+        try {
+            file = await fs.promises.readFile(this.filename, "utf-8");
+            return JSON.parse(file);
+        } catch (error) {
+            if (error.code === "ENOENT") {
+                let emptyArray = [];
+                await fs.promises.writeFile(this.filename, JSON.stringify(emptyArray));
+                return emptyArray;
+            } else {
+                console.error(error);
+            }
+        }
+    }
+}
+
+(async () => {
+    const a = new Contenedor(path.join(__dirname, '..', 'temp', 'products.txt'));
+
+    const indexElement1 = await a.save({
+        "title": "mati",
+        "price": 2,
+        "thumbnail": "url1",
+    });
+
+    const indexElement2 = await a.save({
+        "title": "roy",
+        "price": 3,
+        "thumbnail": "url2",
+    });
+
+    const indexElement3 = await a.save({
+        "title": "marcelo",
+        "price": 28000,
+        "thumbnail": "url4",
+    });
+
+    console.log(`First: ${indexElement1}, second: ${indexElement2}, third: ${indexElement3}`);
+
+    await a.deleteById(indexElement2);
+
+    const updatedList = await a.getAll();
+
+    console.log(updatedList);
+    console.log(updatedList.length);
+})();
