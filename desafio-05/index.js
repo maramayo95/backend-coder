06bd4a11ff9ca10c46f6d5bc4eed43546299e580
Matias <matiasaramayo@hotmail.com>
2022-03-19 03:06:36 -0300

    Terminado pug

--- desafio-05/index.js
+++ desafio-05/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { engine } = require('express/lib/application');
-const res = require('express/lib/response');
-const path = require("path")
-
+const {Router} = express
+const Contenedor = require('./Contenedor')
 const app = express();
+const router = Router()
+
 const port = 8080;
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/static', express.static(__dirname + '/public'))
+app.use('/', router)
+const storeProducts = new Contenedor()
 
 
 app.set('views', './views')
@@ -15,10 +21,7 @@ app.get("/", (req,res )=> {
 })
 app.get("/layout", (req,res)=> {
     res.render('layout.pug', {mensaje: 'Usando pug con express'})
-})
-
-
-
+    })
 
 
 app.listen(port, ()=> {
